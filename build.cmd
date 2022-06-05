2e527fab746bbaaef54a7aa8c79c2721e2ac484c
Seb-Dz <sebastienduruz@protonmail.com>
2022-06-05 17:02:18 +0200

    resolve issue with output folder

--- build.cmd
+++ build.cmd
@@ -4,6 +4,7 @@ echo "Restore & Build the project"
 cd CryptoManager
 dotnet restore
 dotnet build
+cd ../
 
 echo "Invoke electronize build Windows"
 electronize build /target win /dotnet-configuration release
