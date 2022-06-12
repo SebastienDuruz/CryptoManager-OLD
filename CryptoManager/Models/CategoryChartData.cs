4d180d0d5681ecdbc1106f376fc1ad9767612d34
Seb-Dz <sebastienduruz@protonmail.com>
2022-06-12 15:29:48 +0200

    Add charts to statistics page

---
+++ CryptoManager/Models/CategoryChartData.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoManager.Models
+{
+    public class CategoryChartData
+    {
+        public string CategoryName { get; set; }
+        public double TotalValue { get; set; }
+    }
+}
